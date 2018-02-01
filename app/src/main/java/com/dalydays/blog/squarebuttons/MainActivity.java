4e1502f99b9461acbcb9ace61edabcea4b58dbaf
linucksrox <ericdalyblog@dalydays.com>
2018-02-01 14:12:44 -0500

    Tweak light off color and make button spacing a dimension

--- app/src/main/java/com/dalydays/blog/squarebuttons/MainActivity.java
+++ app/src/main/java/com/dalydays/blog/squarebuttons/MainActivity.java
@@ -3,7 +3,6 @@ package com.dalydays.blog.squarebuttons;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
