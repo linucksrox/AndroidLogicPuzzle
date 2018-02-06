2e0c14755cac6e2366a34345c0b158757a241659
linucksrox <ericdalyblog@dalydays.com>
2018-02-06 10:09:57 -0500

    Renamed activity_main.xml to puzzle_board.xml

--- app/src/main/java/com/dalydays/blog/lightsout/PuzzleBoardActivity.java
+++ app/src/main/java/com/dalydays/blog/lightsout/PuzzleBoardActivity.java
@@ -38,7 +38,7 @@ public class PuzzleBoardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.puzzle_board);
 
         // Get a handle on all buttons
         // row a
